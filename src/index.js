10bd15f71ea06a4689710651a4c65b1391e656ae
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2025-02-12 16:54:17 +0700

    feat: Add add function to sum two numbers and log the result

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@
  * This is the main entry point of the application.
  */
 console.log('Hello, world!');
+
+/**
+ * Sum two numbers.
+ * 
+ * @param {number} a The first number to add
+ * @param {number} b The second number to add
+ * @returns The sum of the two numbers
+ */
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 2));
